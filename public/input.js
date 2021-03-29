38a9e5a6eafc2548918e1c4758fb3e2cdc8ec5f1
Glitch (trackstar) <none>
2021-03-29 17:52:51 +0000

    Initial mobile support and other bits and pieces

---
+++ public/input.js
@@ -0,0 +1,28 @@
+const keysDown = {};
+
+window.addEventListener('keydown', e => {
+  keysDown[e.which] = true;
+});
+
+window.addEventListener('keyup', e => {
+  keysDown[e.which] = false;
+});
+
+const mouseDown = [false, false, false];
+
+window.addEventListener('mousedown', e => {
+  e.stopPropagation();
+  mouseDown[e.button] = true;
+});
+
+window.addEventListener('mouseup', e => {
+  e.stopPropagation();
+  mouseDown[e.button] = false;
+});
+
+window.addEventListener('contextmenu', e => { 
+  e.preventDefault(); 
+  return false; 
+});
+
+const touches = [false, false, false, false];
