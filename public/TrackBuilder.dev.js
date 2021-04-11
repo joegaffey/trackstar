74349992f3846f8accbd94cc02cac59e8994e8a0
Glitch (trackstar) <none>
2021-04-11 14:05:29 +0000

    Added teaser 'Go Race' button to editor

--- public/TrackBuilder.dev.js
+++ public/TrackBuilder.dev.js
@@ -290,17 +290,18 @@ class EditorUIScene extends Phaser.Scene {
 
   create () {    
     this.editorScene = this.game.scene.scenes[0];
-    this.addControlPointsButton();
-    this.addCloseLoopButton();
+    this.addControlPointsButton(0);
+    this.addCloseLoopButton(1);
+    this.addRaceButton(2);
   }
   
   update() {    
   }
   
-  addControlPointsButton() {
-    const box = this.add.rectangle(20, 20, 125, 50, 0x000000).setOrigin(0,0);
+  addControlPointsButton(position) {
+    const box = this.add.rectangle(position * 130 + 20, 20, 125, 50, 0x000000).setOrigin(0,0);
     box.alpha = 0.6;
-    this.controlsOnText = this.add.text(35, 35, 'Controls Off', { font: '16px Helvetica', fill: '#aaaaaa' }).setOrigin(0,0);;
+    this.controlsOnText = this.add.text(position * 130 + 35, 35, 'Controls Off', { font: '16px Helvetica', fill: '#aaaaaa' }).setOrigin(0,0);;
     box.setInteractive({ draggable: false });
     box.on('pointerup', (pointer) => {
       this.editorScene.showControls = !this.editorScene.showControls;
@@ -312,16 +313,31 @@ class EditorUIScene extends Phaser.Scene {
     });
   }
   
-  addCloseLoopButton() {
-    const box = this.add.rectangle(150, 20, 125, 50, 0x000000).setOrigin(0,0);
+  addCloseLoopButton(position) {
+    const box = this.add.rectangle(position * 130 + 20, 20, 125, 50, 0x000000).setOrigin(0,0);
     box.alpha = 0.6;
-    const closeLoopText = this.add.text(165, 35, 'Close Loop', { font: '16px Helvetica', fill: '#aaaaaa' }).setOrigin(0,0);;
+    const closeLoopText = this.add.text(position * 130 + 35, 35, 'Close Loop', { font: '16px Helvetica', fill: '#aaaaaa' }).setOrigin(0,0);;
     box.setInteractive({ draggable: false });
     box.on('pointerup', (pointer) => {
       if(this.editorScene.points.length > 4)
         this.editorScene.closeLoop();
       else
         alert('Not enough points! Click on the background.')
+    });
+  }
+  
+   addRaceButton(position) {
+    const box = this.add.rectangle(position * 130 + 20, 20, 125, 50, 0x000000).setOrigin(0,0);
+    box.alpha = 0.6;
+    const closeLoopText = this.add.text(position * 130 + 35, 35, 'Go Race!', { font: '16px Helvetica', fill: '#aaaaaa' }).setOrigin(0,0);;
+    box.setInteractive({ draggable: false });
+    box.on('pointerup', (pointer) => {
+      if(this.editorScene.points.length < 5)
+        alert('Not enough points! Click on the background.');
+      else if(this.editorScene.isOpen)
+        alert('Can\'t race on an open circuit.');
+      else
+        alert('Coming soon :)');
     });
   }
 }
